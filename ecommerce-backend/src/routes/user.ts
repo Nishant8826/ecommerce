e48080c138c326c59893a0d51cbdcbec2a9bec60
Nishant Rathore <rnishant428@gmail.com>
2024-08-29 00:36:46 +0530

    multer and user api's

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { newUser } from "../controllers/user.js";
+import { getAllUsers, newUser, getUser, deleteUser } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 const app = express.Router();
 
-
 app.post('/new', newUser);
+app.get('/getall', adminOnly, getAllUsers);
+app.get('/:id', getUser)
+app.delete('/:id', adminOnly, deleteUser)
 
 
 export default app;
