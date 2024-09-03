a01d4133a66c1734c03bb68cfff8c187af5d4015
Nishant Rathore <rnishant428@gmail.com>
2024-09-03 10:37:06 +0530

    product api

--- ecommerce-backend/src/controllers/product.ts
+++ ecommerce-backend/src/controllers/product.ts
@@ -19,3 +19,60 @@ export const newProduct = TryCatch(async (req: Request<{}, {}, NewProduct>, res,
     const result = await Product.create({ name, price, stock, category: category.toLowerCase(), photo: photo.path })
     return res.status(201).send({ succes: true, result: result, msg: 'product created successfully' });
 });
+
+export const getLatestProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({}).sort({ createdAt: - 1 }).limit(5);
+    return res.status(200).send({ succes: true, result: products });
+});
+
+export const getAllCategories = TryCatch(async (req, res, next) => {
+    const categories = await Product.distinct("category");
+    return res.status(200).send({ succes: true, result: categories });
+});
+
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({});
+    return res.status(200).send({ succes: true, result: products });
+});
+
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) return next(new ErrorHandler(`Product Not Found`, 404));
+
+    return res.status(200).send({ succes: true, result: product });
+});
+
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
+
+    const product = await Product.findById(id);
+    if (!product) return next(new ErrorHandler(`Product Not Found`, 404));
+
+    if (photo) {
+        rm(product.photo, () => {
+            console.log('Old Photo Deleted');
+        })
+        product.photo = photo.path;
+    };
+
+    if (name) product.name = name;
+    if (price) product.price = price;
+    if (stock) product.stock = stock;
+    if (category) product.category = category;
+
+    const result = await product.save();
+    return res.status(201).send({ succes: true, result: result, msg: 'product updated successfully' });
+});
+
+
+export const deleteSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) return next(new ErrorHandler(`Product Not Found`, 404));
+    rm(product.photo!, () => {
+        console.log('Product Photo Deleted');
+    })
+    await Product.deleteOne();
+    return res.status(200).send({ succes: true, msg: 'Product deleted Successfully' });
+});
--- ecommerce-backend/src/routes/product.ts
+++ ecommerce-backend/src/routes/product.ts
@@ -1,8 +1,15 @@
 import express from "express";
-import { newProduct } from "../controllers/product.js";
+import { deleteSingleProduct, getAdminProducts, getAllCategories, getLatestProducts, getSingleProduct, newProduct, updateProduct } from "../controllers/product.js";
 import { singleUpload } from "../middlewares/multer.js";
+import { adminOnly } from "../middlewares/auth.js";
 const app = express.Router();
 
-app.post('/new', singleUpload, newProduct);
+app.post('/new', adminOnly, singleUpload, newProduct);
+app.get('/latest', getLatestProducts);
+app.get('/categories', getAllCategories);
+app.get('/admin-products', adminOnly, getAdminProducts);
+app.get('/search', getAdminProducts);
+
+app.route('/:id').get(getSingleProduct).put(adminOnly, singleUpload, updateProduct).delete(adminOnly, deleteSingleProduct)
 
 export default app;
